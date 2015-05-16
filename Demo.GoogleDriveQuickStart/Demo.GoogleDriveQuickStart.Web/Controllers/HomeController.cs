74bf2c76a894ddca27d8a7b8adfa7758d60316b6
dance2die <dontbothermewithspam@gmail.com>
2015-05-15 21:35:31 -0400

    Added a HomeController (Main entry point)

---
+++ Demo.GoogleDriveQuickStart/Demo.GoogleDriveQuickStart.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.GoogleDriveQuickStart.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		/// <summary>
+		/// Display Top Level Google Drive Folders
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
